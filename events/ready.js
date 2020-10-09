611cc0e1063d1278608f9a7e8f6fc068c2814f18
Glitch (shrub-circular-homburg) <none>
2020-10-09 10:19:26 +0000

    🌱🌱 Checkpoint ./events/ready.js:23468138/2 ./install.bat:23468138/2 ./package.json:23468138/1123

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("s!help", {
+  await client.user.setActivity("h!help", {
     type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
